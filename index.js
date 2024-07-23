bfd6ed4fbef93ceeec12a5664e51929bed880fc3
allan <4lwachira@gmail.com>
2024-07-23 10:59:21 +0300

    add Dom

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function renderOneStock(stock) {
-  const card = document.createElement("li");
-  card.className = "card";
-  card.innerHTML = `
+document.addEventListener("DOMContentLoaded", () => {
+  function renderOneStock(stock) {
+    const card = document.createElement("li");
+    card.className = "card";
+    card.innerHTML = `
   <h5>${stock.title}</h5>
       <img src="${stock.image}" alt="${stock.title}">
       <div class="content">
@@ -13,42 +14,43 @@ function renderOneStock(stock) {
       </div>
   `;
 
-  const btn = card.querySelector(".purchaseBtn");
-  btn.addEventListener("click", handlePurchase);
-  btn.addEventListener("dblclick", () => {
-    btn.style.color= "white";
-    btn.textContent = "purchase";
-  });
-  card.querySelector(".remBtn").addEventListener("click", () => {
-    card.remove();
-    deleteStock(stock.id);
-  });
+    const btn = card.querySelector(".purchaseBtn");
+    btn.addEventListener("click", handlePurchase);
+    btn.addEventListener("dblclick", () => {
+      btn.style.color = "white";
+      btn.textContent = "purchase";
+    });
+    card.querySelector(".remBtn").addEventListener("click", () => {
+      card.remove();
+      deleteStock(stock.id);
+    });
 
-  document.querySelector("#stockList").appendChild(card);
-}
+    document.querySelector("#stockList").appendChild(card);
+  }
 
-function handlePurchase(event) {
-  const btn = event.target;
-  btn.style.color = "red";
-  btn.textContent = "purchased"
-  btn.style.color = "red";
-}
+  function handlePurchase(event) {
+    const btn = event.target;
+    btn.style.color = "red";
+    btn.textContent = "purchased";
+    btn.style.color = "red";
+  }
 
-function getAllStock() {
-  fetch("https://project-1-backend-sigma.vercel.app/stock")
-    .then((res) => res.json())
-    .then((stockData) => stockData.forEach((stock) => renderOneStock(stock)));
-}
-function deleteStock(id) {
-  fetch(`https://project-1-backend-sigma.vercel.app/stock${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
+  function getAllStock() {
+    fetch("https://project-1-backend-sigma.vercel.app/stock")
+      .then((res) => res.json())
+      .then((stockData) => stockData.forEach((stock) => renderOneStock(stock)));
+  }
+  function deleteStock(id) {
+    fetch(`https://project-1-backend-sigma.vercel.app/stock${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-function initialize() {
-  getAllStock();
-}
-initialize();
+  function initialize() {
+    getAllStock();
+  }
+  initialize();
+});
